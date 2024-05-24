5f7fdf7b3328516709057acfc06f825662763503
daniMartinAlo <danielmartinalonso90@gmail.com>
2024-05-24 15:13:29 +0200

    add class Tarea

--- taskManager.py
+++ taskManager.py
@@ -2,4 +2,16 @@ import tkinter as tk
 from tkinter import messagebox
 
 class Tarea:
+    def __init__(self, num, name, desc):
+        self.num = num
+        self.name = name
+        self.desc = desc
+        self.completada = False
+    
+    def completarTarea(self):
+        self.completada = True
+    
+    def __str__(self):
+        estado = "Completada" if self.completada else "Pendiente"
+        return f"Tarea {self.num}: {self.name} - {estado}"
     
